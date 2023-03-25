fcbcdeb835d6883d56923d37b34608e66f6dd935
MauroRussi <89103402+MauroRussi@users.noreply.github.com>
2023-03-25 20:22:01 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,15 +2,15 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Hello From Home!" }');
+    res.send('{ "response": "Hello From Github!" }');
 });
 
 app.get('/will', function (req, res) {
-    res.send('{ "response": "Hello World" }');
+    res.send('{ "response": "Hello Github" }');
 });
 
 app.get('/ready', function (req, res) {
-    res.send('{ "response": " Great!, It works!" }');
+    res.send('{ "response": " Great!, It works in Github!" }');
 });
 
 app.listen(process.env.PORT || 3000, () => {
